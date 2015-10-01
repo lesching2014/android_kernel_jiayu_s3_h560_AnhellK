c15b00b4e242a641eacf50b30726f3ed79585f48
Anmin Hsu <anmin.hsu@mediatek.com>
2015-10-01 23:46:42 +0800

    Fuel Gauge: fix suspend/resume calculation
    [Detail]
    1. bat volt is wrong in the fuel gauge algorithm of suspend/resume
    2. fuel gauge log cannot print out
    [Solution]
    1. fix correct bat volt in resume
    2. fix fuel gauge log level
    [Feature] Battery Management
    
    MTK-Commit-Id: 81ea0e76922c6280d1f7dcdb601d6ba04cf21369
    
    Change-Id: Ibfd975bc72a373c8b15a0737361c50b7054fa865
    Signed-off-by: Hsin-Hsiung Wang <hsin-hsiung.wang@mediatek.com>
    CR-Id: ALPS02314143
    (cherry picked from commit bd1c50b3720b0e12f3e6b8d0fb32dcab1ca41461)
    Backported for 3.10
    Signed-off-by: Mister Oyster <oysterized@gmail.com>
    
    # Conflicts:
    #	drivers/power/mediatek/battery_meter.c

--- drivers/power/mediatek/battery_meter.c
+++ drivers/power/mediatek/battery_meter.c
@@ -3866,7 +3866,6 @@ kal_int32 get_dynamic_period(int first_use, int first_wakeup_time, int battery_c
 	static kal_int32 last_time;
 
 	kal_int32 ret_val = -1;
-	int check_fglog = 0;
 	kal_int32 I_sleep = 0;
 	kal_int32 new_time = 0;
 	kal_int32 vbat_val = 0;
@@ -3911,7 +3910,6 @@ kal_int32 get_dynamic_period(int first_use, int first_wakeup_time, int battery_c
 			*/
 
 			ret = battery_meter_ctrl(BATTERY_METER_CMD_GET_HW_FG_CURRENT, &I_sleep);
-
 			I_sleep = I_sleep / 10;
 			/*
 			if (check_fglog == 0)
@@ -4613,10 +4611,10 @@ static ssize_t fgadc_log_write(struct file *filp, const char __user *buff,
 
 	if (proc_fgadc_data == '1') {
 		bm_print(BM_LOG_CRTI, "enable FGADC driver log system\n");
-		Enable_FGADC_LOG = 1;
+		Enable_FGADC_LOG = BM_LOG_CRTI;
 	} else if (proc_fgadc_data == '2') {
 		bm_print(BM_LOG_CRTI, "enable FGADC driver log system:2\n");
-		Enable_FGADC_LOG = 2;
+		Enable_FGADC_LOG = BM_LOG_FULL;
 	} else {
 		bm_print(BM_LOG_CRTI, "Disable FGADC driver log system\n");
 		Enable_FGADC_LOG = 0;
