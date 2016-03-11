42af9dd5445514223a42b098fb6d8f6923f1491a
Jaegeuk Kim <jaegeuk@kernel.org>
2016-03-11 15:33:22 -0800

    f2fs: submit node page write bios when really required
    If many threads calls fsync with data writes, we don't need to flush every
    bios having node page writes.
    The f2fs_wait_on_page_writeback will flush its bios when the page is really
    needed.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -1279,7 +1279,7 @@ int sync_node_pages(struct f2fs_sb_info *sbi, nid_t ino,
 	pgoff_t index, end;
 	struct pagevec pvec;
 	int step = ino ? 2 : 0;
-	int nwritten = 0, wrote = 0;
+	int nwritten = 0;
 
 	pagevec_init(&pvec, 0);
 
@@ -1369,8 +1369,6 @@ continue_unlock:
 
 			if (NODE_MAPPING(sbi)->a_ops->writepage(page, wbc))
 				unlock_page(page);
-			else
-				wrote++;
 
 			if (--wbc->nr_to_write == 0)
 				break;
@@ -1387,14 +1385,6 @@ continue_unlock:
 	if (step < 2) {
 		step++;
 		goto next_step;
-	}
-
-	if (wrote) {
-		if (ino)
-			f2fs_submit_merged_bio_cond(sbi, NULL, NULL,
-							ino, NODE, WRITE);
-		else
-			f2fs_submit_merged_bio(sbi, NODE, WRITE);
 	}
 	return nwritten;
 }
