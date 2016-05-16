d744ed5d47580b121523bb3d6eedba61cb10b282
Herbert Xu <herbert@gondor.apana.org.au>
2016-05-16 17:28:16 +0800

    netlink: Fix dump skb leak/double free
    When we free cb->skb after a dump, we do it after releasing the
    lock.  This means that a new dump could have started in the time
    being and we'll end up freeing their skb instead of ours.
    
    This patch saves the skb and module before we unlock so we free
    the right memory.
    
    Change-Id: Icdf8adc86b334c32d2f820b5b203989b294fb19f
    Fixes: 16b304f3404f ("netlink: Eliminate kmalloc in netlink dump operation.")
    Reported-by: Baozeng Ding <sploving1@gmail.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Acked-by: Cong Wang <xiyou.wangcong@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    # Conflicts:
    #	net/netlink/af_netlink.c

--- net/netlink/af_netlink.c
+++ net/netlink/af_netlink.c
@@ -2474,6 +2474,7 @@ static int netlink_dump(struct sock *sk)
 	struct netlink_callback *cb;
 	struct sk_buff *skb = NULL;
 	struct nlmsghdr *nlh;
+	struct module *module;
 	int len, err = -ENOBUFS;
 	int alloc_size;
 
@@ -2522,11 +2523,13 @@ static int netlink_dump(struct sock *sk)
 
 	if (cb->done)
 		cb->done(cb);
-	nlk->cb = NULL;
-	mutex_unlock(nlk->cb_mutex);
 
-	module_put(cb->module);
-	netlink_consume_callback(cb);
+	nlk->cb_running = false;
+	module = cb->module;
+	skb = cb->skb;
+	mutex_unlock(nlk->cb_mutex);
+	module_put(module);
+	consume_skb(skb);
 	return 0;
 
 errout_skb:
