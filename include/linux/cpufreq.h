398b953f0d03bf644028305be22e9be2fcb38677
Sandeep Sethi <adeveloper79@gmail.com>
2017-01-07 09:55:43 +0530

    Add Renice and PegasusQ

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -415,6 +415,12 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
 extern struct cpufreq_governor cpufreq_gov_wheatley;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_wheatley)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
+extern struct cpufreq_governor cpufreq_gov_pegasusq;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_RENICE)
+extern struct cpufreq_governor cpufreq_gov_renice;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_renice)
 #endif
 
 
