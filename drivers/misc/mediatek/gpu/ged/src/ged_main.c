2375a5bed0a60deef11d3168efff502747c144b2
lulu2 <lulu2@lenovo.com>
2017-07-07 15:28:51 +0800

    mtk: ged: heapoverflow fix
    Patch Type:
      Customer Request
    CR ID:
      ALPS03342077
    Severity:
      Critical
    Description:
      [Buganizer]Security Vulnerability 37205193 - A ***6797 driver bug(ged_HeapOverflow)
      [[Title for Customer]]
      [Buganizer]Security Vulnerability 37205193 - A ***6797 driver bug(ged_HeapOverflow)
      [[Problem Description]]
      [Buganizer]Security Vulnerability 37205193 - A ***6797 driver bug(ged_HeapOverflow)
      [[Potential Impa*** of the solution]]
      N/A
      [[Modules to be verified after taking p***h]]
      N/A
      [[問題標題]]
      [Buganizer]Security Vulnerability 37205193 - A ***6797 driver bug(ged_HeapOverflow)
      [[問題現象]]
      [Buganizer]Security Vulnerability 37205193 - A ***6797 driver bug(ged_HeapOverflow)
      [[解法可能帶來的影響]]
      (請填寫於此行下方，並描述如果合入這個p***h可能會有什麼trade off的改變，如perfo******e降低、UI改變等等)
      N/A
      [[建議驗證模塊]]
      (請填寫於此行下方，並建議客戶合了此p***h後要驗證哪些module或feature)
      N/A
      [[Title for Customer]]
      Security Vulnerability
      [[Problem Description]]
      ged_HeapOverflow / kernel exception
      [[Potential Impa*** of the solution]]
      none
      [[Modules to be verified after taking p***h]]
      OpenGL|ES releated
      [[問題標題]]
      Security Vulnerability
      [[問題現象]]
      ged_HeapOverflow / kernel exception
      [[解法可能帶來的影響]]
      (請填寫於此行下方，並描述如果合入這個p***h可能會有什麼trade off的改變，如perfo******e降低、UI改變等等)
      無
      [[建議驗證模塊]]
      (請填寫於此行下方，並建議客戶合了此p***h後要驗證哪些module或feature)
      OpenGL|ES 相關N/A
    
    Associated Files:
      kernel-3.18/drivers/misc/mediatek/gpu/ged/src/ged_main.c
    
    Signed-off-by: Mister Oyster <oysterized@gmail.com>
    
    # Conflicts:
    #	drivers/misc/mediatek/gpu/ged/src/ged_main.c

--- drivers/misc/mediatek/gpu/ged/src/ged_main.c
+++ drivers/misc/mediatek/gpu/ged/src/ged_main.c
@@ -113,129 +113,74 @@ static long ged_dispatch(struct file *pFile, GED_BRIDGE_PACKAGE *psBridgePackage
 	 * if and only if they are both zero in a 2's complement numeral system.
 	 * That is: if overflow happen, the sum will be a negative number.
 	 */
-	if (psBridgePackageKM->i32InBufferSize >= 0 && psBridgePackageKM->i32OutBufferSize >= 0
-			&& psBridgePackageKM->i32InBufferSize + psBridgePackageKM->i32OutBufferSize >= 0
-			&& psBridgePackageKM->i32InBufferSize + psBridgePackageKM->i32OutBufferSize
-			< GED_IOCTL_PARAM_BUF_SIZE)
-	{
-		pvInt = gvIOCTLParamBuf;
-		pvOut = (void *)((char *)pvInt + (uintptr_t)psBridgePackageKM->i32InBufferSize);
-		if (psBridgePackageKM->i32InBufferSize > 0)
-		{
-			if (0 != ged_copy_from_user(pvInt, psBridgePackageKM->pvParamIn, psBridgePackageKM->i32InBufferSize))
-			{
-				GED_LOGE("ged_copy_from_user fail\n");
-				return ret;
-			}
-		}
+    if (psBridgePackageKM->i32InBufferSize >= 0 && psBridgePackageKM->i32OutBufferSize >= 0
+            && psBridgePackageKM->i32InBufferSize + psBridgePackageKM->i32OutBufferSize >= 0
+            && psBridgePackageKM->i32InBufferSize + psBridgePackageKM->i32OutBufferSize
+            < GED_IOCTL_PARAM_BUF_SIZE)
+    {
+        pvInt = gvIOCTLParamBuf;
+        pvOut = (void *)((char *)pvInt + (uintptr_t)psBridgePackageKM->i32InBufferSize);
+        if (psBridgePackageKM->i32InBufferSize > 0)
+        {
+            if (0 != ged_copy_from_user(pvInt, psBridgePackageKM->pvParamIn, psBridgePackageKM->i32InBufferSize))
+            {
+                GED_LOGE("ged_copy_from_user fail\n");
+                return ret;
+            }
+        }
 
-		// we will change the below switch into a function pointer mapping table in the future
-		switch (GED_GET_BRIDGE_ID(psBridgePackageKM->ui32FunctionID))
-		{
-			case GED_BRIDGE_COMMAND_LOG_BUF_GET:
-				pFunc = (ged_bridge_func_type*)ged_bridge_log_buf_get;
-				break;
-			case GED_BRIDGE_COMMAND_LOG_BUF_WRITE:
-				pFunc = (ged_bridge_func_type*)ged_bridge_log_buf_write;
-				break;
-			case GED_BRIDGE_COMMAND_LOG_BUF_RESET:
-				pFunc = (ged_bridge_func_type*)ged_bridge_log_buf_reset;
-				break;
-			case GED_BRIDGE_COMMAND_BOOST_GPU_FREQ:
-				pFunc = (ged_bridge_func_type*)ged_bridge_boost_gpu_freq;
-				break;
-			case GED_BRIDGE_COMMAND_MONITOR_3D_FENCE:
-				pFunc = (ged_bridge_func_type*)ged_bridge_monitor_3D_fence;
-				break;
-			case GED_BRIDGE_COMMAND_QUERY_INFO:
-				pFunc = (ged_bridge_func_type*)ged_bridge_query_info;
-				break;
-			case GED_BRIDGE_COMMAND_NOTIFY_VSYNC:
-				pFunc = (ged_bridge_func_type*)ged_bridge_notify_vsync;
-				break;
-			case GED_BRIDGE_COMMAND_DVFS_PROBE:
-				pFunc = (ged_bridge_func_type*)ged_bridge_dvfs_probe;
-				break;
-			case GED_BRIDGE_COMMAND_DVFS_UM_RETURN:
-				pFunc = (ged_bridge_func_type*)ged_bridge_dvfs_um_retrun;
-				break;
-			case GED_BRIDGE_COMMAND_EVENT_NOTIFY:
-				pFunc = (ged_bridge_func_type*)ged_bridge_event_notify;
-				break;
-#ifdef ENABLE_FRR_FOR_MT6XXX_PLATFORM
-            case GED_BRIDGE_COMMAND_VSYNC_WAIT:
-				pFunc = (ged_bridge_func_type*)ged_bridge_vsync_wait;
-				break;
-#endif
-			case GED_BRIDGE_COMMAND_GE_ALLOC:
-				pFunc = (ged_bridge_func_type *)ged_bridge_ge_alloc;
-				break;
-			case GED_BRIDGE_COMMAND_GE_RETAIN:
-				pFunc = (ged_bridge_func_type *)ged_bridge_ge_retain;
-				break;
-			case GED_BRIDGE_COMMAND_GE_RELEASE:
-				pFunc = (ged_bridge_func_type *)ged_bridge_ge_release;
-				break;
-			case GED_BRIDGE_COMMAND_GE_GET:
-				pFunc = (ged_bridge_func_type *)ged_bridge_ge_get;
-				break;
-			case GED_BRIDGE_COMMAND_GE_SET:
-				pFunc = (ged_bridge_func_type *)ged_bridge_ge_set;
-				break;
-			default:
-				GED_LOGE("Unknown Bridge ID: %u\n", GED_GET_BRIDGE_ID(psBridgePackageKM->ui32FunctionID));
-				break;
-		}
+		/* Make sure that the UM will never break the KM.
+		 * Check IO size are both matched the size of IO sturct.
+		 */
+#define SET_FUNC_AND_CHECK(func, struct_name) do { \
+	pFunc = (ged_bridge_func_type *) func; \
+	if (sizeof(GED_BRIDGE_IN_##struct_name) > psBridgePackageKM->i32InBufferSize || \
+			sizeof(GED_BRIDGE_OUT_##struct_name) > psBridgePackageKM->i32OutBufferSize) { \
+		GED_LOGE("GED_BRIDGE_COMMAND_##cmd fail io_size:%d/%d, expected: %zu/%zu", \
+				psBridgePackageKM->i32InBufferSize, psBridgePackageKM->i32OutBufferSize, \
+				sizeof(GED_BRIDGE_IN_##struct_name), sizeof(GED_BRIDGE_OUT_##struct_name)); \
+		goto dispatch_exit; \
+	} } while (0)
 
-		if (pFunc)
-		{
-			ret = pFunc(pvInt, pvOut);
-		}
+        // we will change the below switch into a function pointer mapping table in the future
+        switch(GED_GET_BRIDGE_ID(psBridgePackageKM->ui32FunctionID))
+        {
+        case GED_BRIDGE_COMMAND_LOG_BUF_GET:
+            SET_FUNC_AND_CHECK(ged_bridge_log_buf_get, LOGBUFGET);
+            break;
+        case GED_BRIDGE_COMMAND_LOG_BUF_WRITE:
+            SET_FUNC_AND_CHECK(ged_bridge_log_buf_write, LOGBUFWRITE);
+            break;
+        case GED_BRIDGE_COMMAND_LOG_BUF_RESET:
+            SET_FUNC_AND_CHECK(ged_bridge_log_buf_reset, LOGBUFRESET);
+            break;            
+        case GED_BRIDGE_COMMAND_BOOST_GPU_FREQ:
+            SET_FUNC_AND_CHECK(ged_bridge_boost_gpu_freq, BOOSTGPUFREQ);
+            break;
+        case GED_BRIDGE_COMMAND_MONITOR_3D_FENCE:
+            SET_FUNC_AND_CHECK(ged_bridge_monitor_3D_fence, MONITOR3DFENCE);
+            break;
+        default:
+            GED_LOGE("Unknown Bridge ID: %u\n", GED_GET_BRIDGE_ID(psBridgePackageKM->ui32FunctionID));
+            break;
+        }
 
-		switch (GED_GET_BRIDGE_ID(psBridgePackageKM->ui32FunctionID)) {
-		case GED_BRIDGE_COMMAND_GE_ALLOC:
-			{
-				GED_BRIDGE_OUT_GE_ALLOC *out = (GED_BRIDGE_OUT_GE_ALLOC *)pvOut;
+        if (pFunc)
+        {
+            ret = pFunc(pvInt, pvOut);
+        }
 
-				if (out->eError == GED_OK) {
-					ged_ge_init_context(&pFile->private_data);
-					ged_ge_context_ref(pFile->private_data, out->ge_hnd);
-				}
-			}
-			break;
-		case GED_BRIDGE_COMMAND_GE_RETAIN:
-			{
-				GED_BRIDGE_IN_GE_RETAIN *in = (GED_BRIDGE_IN_GE_RETAIN *)pvInt;
-				GED_BRIDGE_OUT_GE_RETAIN *out = (GED_BRIDGE_OUT_GE_RETAIN *)pvOut;
+        if (psBridgePackageKM->i32OutBufferSize > 0)
+        {
+            if (0 != ged_copy_to_user(psBridgePackageKM->pvParamOut, pvOut, psBridgePackageKM->i32OutBufferSize))
+            {
+                return ret;
+            }
+        }
+    }
 
-				if (out->eError == GED_OK) {
-					ged_ge_init_context(&pFile->private_data);
-					ged_ge_context_ref(pFile->private_data, in->ge_hnd);
-				}
-			}
-			break;
-		case GED_BRIDGE_COMMAND_GE_RELEASE:
-			{
-				GED_BRIDGE_IN_GE_RELEASE *in = (GED_BRIDGE_IN_GE_RELEASE *)pvInt;
-				GED_BRIDGE_OUT_GE_RELEASE *out = (GED_BRIDGE_OUT_GE_RELEASE *)pvOut;
-
-				if (out->eError == GED_OK)
-					ged_ge_context_deref(pFile->private_data, in->ge_hnd);
-
-			}
-			break;
-		}
-
-		if (psBridgePackageKM->i32OutBufferSize > 0)
-		{
-			if (0 != ged_copy_to_user(psBridgePackageKM->pvParamOut, pvOut, psBridgePackageKM->i32OutBufferSize))
-			{
-				return ret;
-			}
-		}
-	}
-
-	return ret;
+dispatch_exit:
+    return ret;
 }
 
 DEFINE_SEMAPHORE(ged_dal_sem);
