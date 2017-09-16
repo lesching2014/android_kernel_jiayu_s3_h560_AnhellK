1c6b1330c6b90d6833f7168c78c9a449e3b5f040
sandstranger <sandstranger@gmail.com>
2017-09-16 23:05:04 +0400

    use gcc 4.9 again (reverted from commit 7ce6e16abb03b86b755c93c06652a7d961864180) (reverted from commit 09f24a17ef42ad74c3ede8b7294ff394fab3ed9f)

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 CURRENT_DIR=$PWD
 export ARCH=arm64
 export SUBARCH=arm
-export CROSS_COMPILE=/home/sandstranger/Android/utility/gcc-prebuilts/bin/aarch64-linaro-linux-android-
+export CROSS_COMPILE=/home/sandstranger/Android/utility/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 make clean
 make mrproper
 make h560_defconfig
