e52d1a7b4554442f4ef0330d5a9d28e976117a99
sandstranger <sandstranger@gmail.com>
2017-12-24 14:34:54 +0400

    use gcc 5.5 in builing , gcc 6 and 7 painfull for mtk , gcc 4.9 is too old ... (reverted from commit 8898a5182c3172ba6ff69d3589fc7be3eab68900)

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 CURRENT_DIR=$PWD
 export ARCH=arm64
 export SUBARCH=arm
-export CROSS_COMPILE=/home/sandstranger/Android/utility/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+export CROSS_COMPILE=/home/sandstranger/Android/utility/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 make clean
 make mrproper
 make h560_defconfig
