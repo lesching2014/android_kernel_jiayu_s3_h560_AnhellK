3a4922f48102578d89eb5ca98bb667bf86de3581
sandstranger <sandstranger@gmail.com>
2018-01-12 23:15:28 +0400

    mt6735: Add an option to multiplex AP and STA on wlan0
    This adds CONFIG_MTK_COMBO_AOSP_TETHERING_SUPPORT which, when enabled,
    allows ap and wlan to co-exist in the same interface, as Android
    expects.
    
    Most of this functionality is also available (albeit not compilable
    broken)
    under CFG_TC1_FEATURE but that has larger implications around the radio
    and usb stack that we do not want to adopt.
    
    Change-Id: Ib1d1be40566f1bb9ccc7be45b49ec8d1f3b3ba58
    Ticket: PORRIDGE-30
    
    Signed-off-by: danielhk <daniel_hk>
    
    # Conflicts:
    #	drivers/misc/mediatek/connectivity/combo/drv_wlan/mt6630/wlan/os/linux/gl_init.c (reverted from commit 87de2a988b915dfa3a3aeb4dfd14c84526b66c4e)

--- drivers/misc/mediatek/connectivity/combo/drv_wlan/mt6630/wlan/os/linux/gl_init.c
+++ drivers/misc/mediatek/connectivity/combo/drv_wlan/mt6630/wlan/os/linux/gl_init.c
@@ -940,10 +940,6 @@ const UINT_32 mtk_cipher_suites[5] = {
 
 #define NIC_INF_NAME    "wlan%d"	/* interface name */
 
-#if defined(CONFIG_MTK_COMBO_AOSP_TETHERING_SUPPORT)
-#define NIC_INF_NAME_IN_AP_MODE  "legacy%d"
-#endif
-
 #if CFG_SUPPORT_SNIFFER
 #define NIC_MONITOR_INF_NAME	"radiotap%d"
 #endif
@@ -2228,21 +2224,8 @@ static struct wireless_dev *wlanNetCreate(PVOID pvData)
 	kalMemZero(prGlueInfo, sizeof(GLUE_INFO_T));
 	/* 4 <3> Initial Glue structure */
 	/* 4 <3.1> create net device */
-#if defined(CONFIG_MTK_COMBO_AOSP_TETHERING_SUPPORT)
-	if (wlan_if_changed) {
-		prGlueInfo->prDevHandler =
-		    alloc_netdev_mq(sizeof(P_GLUE_INFO_T), NIC_INF_NAME_IN_AP_MODE, ether_setup, CFG_MAX_TXQ_NUM);
-	} else {
-		prGlueInfo->prDevHandler =
-		    alloc_netdev_mq(sizeof(P_GLUE_INFO_T), NIC_INF_NAME, ether_setup, CFG_MAX_TXQ_NUM);
-	}
-#else
 	prGlueInfo->prDevHandler =
 	    alloc_netdev_mq(sizeof(NETDEV_PRIVATE_GLUE_INFO), NIC_INF_NAME, ether_setup, CFG_MAX_TXQ_NUM);
-#endif
-
-//	prGlueInfo->prDevHandler =
-//	    alloc_netdev_mq(sizeof(NETDEV_PRIVATE_GLUE_INFO), NIC_INF_NAME, ether_setup, CFG_MAX_TXQ_NUM);
 
 	DBGLOG(INIT, INFO, "net_device prDev(0x%p) allocated\n", prGlueInfo->prDevHandler);
 	if (!prGlueInfo->prDevHandler) {
