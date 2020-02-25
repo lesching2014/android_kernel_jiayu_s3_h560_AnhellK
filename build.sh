3c16ff5b7065cd54242fde91f6dbfe86a31fcf36
Angel <angelcajim@gmail.com>
2020-02-25 19:35:04 +0100

    Corregido error resolucion pantalla 1024->1080

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 CURRENT_DIR=$PWD
 export ARCH=arm64
 export SUBARCH=arm
-export CROSS_COMPILE=/home/sandstranger/Android/utility/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+#export CROSS_COMPILE=/home/sandstranger/Android/utility/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+export CROSS_COMPILE=/../../../aarch64-linux-android-opt-gnu-8.x/bin/aarch64-opt-linux-android-
 make clean
 make mrproper
 make h560_defconfig
