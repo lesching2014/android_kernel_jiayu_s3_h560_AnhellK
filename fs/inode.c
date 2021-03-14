b9eaff8971c64db62915b85d90558cb70db75b6d
AnhellK <39651508+AnhellK@users.noreply.github.com>
2021-03-15 00:31:57 +0100

    Fix up non-directory creation in SGID directories (corregido)

--- fs/inode.c
+++ fs/inode.c
@@ -1944,7 +1944,7 @@ void inode_init_owner(struct inode *inode, const struct inode *dir,
 		else if ((mode & (S_ISGID | S_IXGRP)) == (S_ISGID | S_IXGRP) &&
 			 !in_group_p(inode->i_gid) &&
 			 !capable_wrt_inode_uidgid(dir, CAP_FSETID))
-
+			mode &= ~S_ISGID;
 	} else
 		inode->i_gid = current_fsgid();
 	inode->i_mode = mode;
